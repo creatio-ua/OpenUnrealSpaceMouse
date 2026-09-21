a93b93adf5db87eddec47404666c96fec3b67f77
Oleksii Dovgodko <alexd.admin@gmail.com>
2026-09-21 17:11:06 +0200

    Revert "Undesired local variable removed"
    This reverts commit d116c819685ef7c6f4623e0b9cb40b7ea8e16545.

--- Source/SpaceMouseReader/Private/SpaceMouseReader/HidDeviceSource.cpp
+++ Source/SpaceMouseReader/Private/SpaceMouseReader/HidDeviceSource.cpp
@@ -83,7 +83,8 @@ namespace SpaceMouse::Reader
 	ranges::any_view<TWeakPtr<FDeviceModel>> FHidDeviceSource::GetKnownHidDevices()
 	{
 		namespace rv = ranges::views;
-		return AsView(GetAllKnownDeviceModels())
+		auto allModels = GetAllKnownDeviceModels();
+		return AsView(allModels)
 			| rv::filter([](TSharedRef<FDeviceModel> const& model) -> bool
 			{
 				return model->TryGet<FCreateHidDevice>() && model->GetId().TryGet<Hid::FHidDeviceId>();
